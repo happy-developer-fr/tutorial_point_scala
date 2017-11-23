5a666ccad14ab198382ca105a7ef95ba8913c5c3
jcottet <jcottet@etai.fr>
2017-11-23 10:10:40 +0100

    ajout point and location

--- src/scala/HelloWorld.scala
+++ src/scala/HelloWorld.scala
@@ -5,4 +5,9 @@ package scala
   */
 object HelloWorld {
 
+  def main(args: Array[String]) {
+    println(
+      """Hello,
+        |world!\b\b"""" stripMargin) // prints Hello World
+  }
 }
--- src/scala/Point.scala
+++ src/scala/Point.scala
@@ -1,8 +1,31 @@
 package scala
 
-/**
-  * Created by jcottet on 22/11/17.
-  */
-class Point {
-
+object PointRun{
+  def main(args:Array[String]) {
+    var l = new Location(1, 2,3)
+    l.move(1, 1,1)
+  }
 }
+
+class Point(val xc: Int, val yc: Int) {
+  var x = xc
+  var y = yc
+
+  def move(dx: Int, dy: Int) {
+    x = x + dx
+    y = y + dy
+    println("Point x location = " + x)
+    println("Point y location = " + y)
+  }
+}
+
+class Location(override val xc: Int, override  val yc:Int, val zc:Int) extends Point(xc,yc){
+  var z = zc
+  def move(dx:Int, dy:Int, dz:Int): Unit ={
+    super.move(dx,dy)
+    z = z + dz
+    println("Point z location = " + z)
+  }
+}
+
+
